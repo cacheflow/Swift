02397503e63d407d86aed5900b9fbfae647af428
Lex Alexander <l.alexander10@gmail.com>
2014-08-10 15:52:09 -0700

    created the ground positio andn physics for the environment.

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -24,14 +24,32 @@ class GameScene: SKScene {
         MarioTexture.filteringMode = SKTextureFilteringMode.Nearest
         
         mario = SKSpriteNode(texture: MarioTexture)
-        mario.setScale(0.5)
-        mario.position = CGPoint(x: self.frame.size.width * 0.35, y: self.frame.size.height * 0.6)
+        mario.setScale(0.7)
+        mario.position = CGPoint(x: self.frame.size.width * 0.5, y: self.frame.size.height * 0.6)
         
         mario.physicsBody = SKPhysicsBody(circleOfRadius:mario.size.height/2.0)
         mario.physicsBody.dynamic = true
         mario.physicsBody.allowsRotation = false
         
         self.addChild(mario)
+        
+        //Ground for Mario 
+        
+        var groundTexture = SKTexture(imageNamed:"ground")
+        
+        var sprite = SKSpriteNode(texture: groundTexture)
+        sprite.setScale(0.5)
+        sprite.position = CGPointMake(self.size.width/2, sprite.size.height/2.0)
+        self.addChild(sprite)
+        
+        
+        var ground = SKNode()
+        
+        ground.position = CGPointMake(0, groundTexture.size().height)
+        ground.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(self.frame.size.width, groundTexture.size().height * 0.1))
+        
+        ground.physicsBody.dynamic = false
+        self.addChild(ground)
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
